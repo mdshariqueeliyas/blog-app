d17ce610b9913c628fa2e2f1a3e5de875a4a3506
mdshariqueeliyas <mdsharique.eliyas@dremio.com>
2023-01-28 17:03:12 +0530

    added all routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './routes/Home';
+import About from './routes/About';
+import Article from './routes/Article';
+import ArticlesList from './routes/ArticlesList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div id="page-body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles/:articleId" element={<Article />} />
+          <Route path="/articles" element={<ArticlesList />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/routes/ArticlesList.js
@@ -0,0 +1,7 @@
+const ArticlesList = () => {
+    return (
+        <h1>This is the articles list page!</h1>
+    );
+}
+
+export default ArticlesList;
