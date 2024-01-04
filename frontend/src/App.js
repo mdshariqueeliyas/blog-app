b5ac9d880da95cb70a0044029ea254af9141bab6
Md Sharique Eliyas <meliyas@purestorage.com>
2024-01-04 23:15:41 +0530

    changed theme and added loading animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
 import Home from './routes/Home';
 import About from './routes/About';
 import Article from './routes/Article';
 import ArticlesList from './routes/ArticlesListPage';
 import NotFoundPage from './routes/NotFound';
+import information from './content/information';
+import { Loading } from './components/Loading';
+import { Suspense, useState, useEffect } from 'react';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1300);
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
+
+  if (isLoading) {
+    return <Loading/>
+  }
+
   return (
     <div className="App">
-      <NavBar />
-      <div id="page-body">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/articles/:articleId" element={<Article />} />
-          <Route path="/articles" element={<ArticlesList />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
+      <Suspense fallback={<Loading />} >
+        <NavBar
+          firstName={information.userData.firstName}
+          lastName={information.userData.lastName}
+        />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/articles/:articleId" element={<Article />} />
+            <Route path="/articles" element={<ArticlesList />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </Suspense>
     </div>
   );
 }
