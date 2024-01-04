36386d152142e9f918e9e50cbc2998c6828f6f4a
Md Sharique Eliyas <meliyas@purestorage.com>
2024-01-04 23:29:17 +0530

    changed theme and added loading animation

--- frontend/src/content/information.js
+++ frontend/src/content/information.js
@@ -4,7 +4,6 @@ const userData = {
     firstName: "<Md Sharique",
     lastName: " Eliyas/>",
     title: "About Me",
-    img: "https://pics.craiyon.com/2023-10-30/66ff0c95358e458c859ecea5d3764b32.webp",
   };
   
   //personal information end
